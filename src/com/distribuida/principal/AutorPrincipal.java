0f9f2da5f647aba8c0714a96e1f6f5012b841041
IA <IA@LAB-SW3-03.ALUMNOS.LOCAL>
2024-11-20 19:45:43 -0500

    Actualizacion

---
+++ src/com/distribuida/principal/AutorPrincipal.java
@@ -0,0 +1,20 @@
+package com.distribuida.principal;
+
+import com.distribuida.entities.Autor;
+
+public class AutorPrincipal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	Autor autor=new Autor(1,"Robert de","Niro","España","Madrid","5748199932","deniro@hotmail.com");
+	System.out.println(autor);
+	
+	
+	
+	
+	
+	
+	}
+
+}
